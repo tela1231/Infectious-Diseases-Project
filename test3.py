95eb09ac14a39dbb900ba2162bf00d3c0c644666
Tehran <tela1231@colorado.edu>
2024-04-28 05:01:25 +0000

    added EW initial condition in test3.py

--- test3.py
+++ test3.py
@@ -9,17 +9,17 @@ import math
 def equations(t, y, beta, rho, sigma, gamma):
     S, IOV, IOT, IW, EOV, EOT, EW, ROV, ROT, RW = y
 
-    # Define vR based on the time t
+    # Define phi based on the time t
     if 144 <= t <= 175 or 206 <= t <= 216:
-        vR = (0.8) * (0.074)
+        phi = (0.8) * (0.074)
     else:
-        vR = 0
+        phi = 0
 
-    S_dot = -(beta * S * rho) * (IOV + IOT) - (beta * S * IW) - (vR * S)
+    S_dot = -(beta * S * rho) * (IOV + IOT) - (beta * S * IW) - (phi * S)
     IOV_dot = sigma * EOV - gamma * IOV
     IOT_dot = sigma * EOT - gamma * (IOT)
     IW_dot = sigma * EW - gamma * IW
-    EOV_dot = (vR * S) - (sigma * EOV)
+    EOV_dot = (phi * S) - (sigma * EOV)
     EOT_dot = (rho * beta * S) * (IOV + IOT) - (sigma * EOT)
     EW_dot = (beta * S * IW) - (sigma * EW)
     ROV_dot = gamma * IOV
@@ -28,25 +28,39 @@ def equations(t, y, beta, rho, sigma, gamma):
 
     return [S_dot, IOV_dot, IOT_dot, IW_dot, EOV_dot, EOT_dot, EW_dot, ROV_dot, ROT_dot, RW_dot]
 
+def find_closest_index(numbers, target):
+    closest_index = None
+    min_difference = float('inf')  # Initialize with positive infinity
+
+    for i, num in enumerate(numbers):
+        difference = abs(num - target)
+        if difference < min_difference:
+            min_difference = difference
+            closest_index = i
+
+    return closest_index
+
 
 # Define initial conditions
 IOV_0 = 0
 IOT_0 = 0
+EOV_0 = 0
+EOT_0 = 0
 ROV_0 = 0
 ROT_0 = 0
 RW_0 = 0
+IW_0 = 0.00434
+EW_0 = 0 #set temp value to EW
+
+# Define the time span
+t_span = [0, 295]
+
+# define star initial conditions for EW estimation
 alpha_star = 0.25
 gamma_star = 0.093
-IW_0 = 0.00434
-IW_00 = 0.001
-EW_0 = (gamma_star / alpha_star) * IW_00
-S0 = 1 - IW_00 - EW_0
-EOV_0 = 0
-EOT_0 = 0
-
-# Initial conditions
-
-y0 = [S0, IOV_0, IOT_0, IW_0, EOV_0, EOT_0, EW_0, ROV_0, ROT_0, RW_0]  
+IW_0_star = 0.001
+EW_0_star = (gamma_star / alpha_star) * IW_0_star
+S0_star = 1 - IW_0_star - EW_0_star
 
 # Define parameter values
 beta = 0.151
@@ -56,21 +70,39 @@ gamma = 0.093
 sewage_scaling_o = 2.02 * (10**7)
 sewage_scaling_w = 1.07 * (10**7)
 
-# Define the time span
-t_span = [0, 295]
+# Star initial conditions
+y0_star = [S0_star, IOV_0, IOT_0, IW_0_star, EOV_0, EOT_0, EW_0_star, ROV_0, ROT_0, RW_0]  
+
+# Solve the differential equations for EW estimation
+star_solutions = solve_ivp(equations, t_span, y0_star, args=(beta, rho, sigma, gamma), dense_output=True)
+
+# Extract the star solutions
+t_values = np.linspace(0, 295, 100)
+y_star_values = star_solutions.sol(t_values)
+
+# Extract the IW_star and EW_star soln
+IW_star_values = y_star_values[3]
+EW_star_values = y_star_values[6]
+
+# Use find_closest_index to find where IW_star is equal to IW_0
+index = find_closest_index(IW_star_values, IW_0)
+
+# Assign the value of EW_0 to the index  from EW_star_values
+EW_0 = EW_star_values[index]
+
+# Determine S0 with new EW_0 value
+S0 = 1 - IW_0 - EW_0
+
+# Set new initial conditions for main modeling
+y0 = [S0, IOV_0, IOT_0, IW_0, EOV_0, EOT_0, EW_0, ROV_0, ROT_0, RW_0]  
 
 # Solve the differential equations
 solution = solve_ivp(equations, t_span, y0, args=(beta, rho, sigma, gamma), dense_output=True)
 
-
-
-# Extract the solution
-t_values = np.linspace(0, 295, 100)
+# Extract the solutions
 y_values = solution.sol(t_values)
 
-
-
-# Extract the solution for S, I, E, and R
+# Extract the star_solutions for S, I, E, and R
 S_values = y_values[0]
 IW_values = y_values[3]
 IOV_values = y_values[1]
@@ -134,4 +166,3 @@ plt.legend()
 plt.grid(True)
 plt.savefig('Recovered_plot.png', bbox_inches='tight')
 plt.close()
-
